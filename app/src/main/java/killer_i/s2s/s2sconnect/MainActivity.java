d56f12076f1827e4e0b3aa6bd1c79b47d8583a6c
Killer-I <ishunfs@gmail.com>
2020-04-21 06:37:48 +0530

    Initial commit

---
+++ app/src/main/java/killer_i/s2s/s2sconnect/MainActivity.java
@@ -0,0 +1,14 @@
+package killer_i.s2s.s2sconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
